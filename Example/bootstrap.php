92dbcf91ca8113da71883e1f47fea65da5790aa6
michal-tupy-defend <michal.tupy@defendinsurance.eu>
2019-04-24 07:56:49 +0200

    Optimise docker

--- Example/bootstrap.php
+++ Example/bootstrap.php
@@ -6,6 +6,12 @@
  * Time: 10:00 AM
  */
 require_once __DIR__ . "/../vendor/autoload.php";
-require_once "constants.php";
+
+$constantsFile = __DIR__ . '/constants.php';
+
+if (is_file($constantsFile)) {
+    require_once $constantsFile   ;
+}
+
 require_once "Helper.php";
 require_once "Authenticate.php";
